9996ebc06c76d0b6735428e25ecb24deb3706754
Oddant1 <oddant1@hotmail.com>
2024-11-14 14:32:54 -0700

    Create config from QIIME 2 test

--- test.py
+++ test.py
@@ -1,4 +1,28 @@
 import parsl
-from parsl.configs.htex_local import config
+
+from parsl.config import Config
+from parsl.channels import LocalChannel
+from parsl.providers import SlurmProvider, AdHocProvider
+from parsl.executors import HighThroughputExecutor
+from parsl.launchers import SrunLauncher
+
+config = Config(
+    strategy='None',
+    executors=[
+        HighThroughputExecutor(
+            label='default',
+            max_workers=10,
+            provider=SlurmProvider(
+                launcher=SrunLauncher(),
+                channel=LocalChannel()),
+        ),
+        HighThroughputExecutor(
+            label='other',
+            max_workers=10,
+            provider=AdHocProvider(
+                channels=LocalChannel()
+            )
+        )
+    ])
 
 parsl.load(config)
