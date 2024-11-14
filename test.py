687d5720196a8de99e7dd5ef72f4c4dd25d2025f
Oddant1 <oddant1@hotmail.com>
2024-11-14 14:23:14 -0700

    Smaller example

--- test.py
+++ test.py
@@ -1,25 +1,4 @@
 import parsl
-
-from parsl.config import Config
-from parsl.providers import SlurmProvider
-from parsl.executors import HighThroughputExecutor
-from parsl.launchers import SrunLauncher
-from parsl.addresses import address_by_interface
-
-config = Config(
-    executors=[
-        HighThroughputExecutor(
-            label="frontera_htex",
-            address=address_by_interface('ib0'),
-            max_workers_per_node=56,
-            provider=SlurmProvider(
-                nodes_per_block=128,
-                init_blocks=1,
-                partition='normal',
-                launcher=SrunLauncher(),
-            ),
-        )
-    ],
-)
+from parsl.configs.htex_local import config
 
 parsl.load(config)
